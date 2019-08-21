e9d43dafc4fb41c1498013cb5aef74bfe683b7c1
Alistair Dutton <alistair.dutton@ovoenergy.com>
2019-08-21 14:06:42 +0100

    Bumped deps.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://github.com/ovotech/ring-jwt"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.8.1"]
-                 [commons-codec "1.12"]
+  :dependencies [[cheshire "5.9.0"]
+                 [commons-codec "1.13"]
                  [org.clojure/clojure "1.10.1"]
-                 [com.auth0/java-jwt "3.8.1"]
-                 [com.auth0/jwks-rsa "0.8.2"]
+                 [com.auth0/java-jwt "3.8.2"]
+                 [com.auth0/jwks-rsa "0.8.3"]
                  [fipp "0.6.18"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0"]
                                   [kelveden/clj-wiremock "1.3.0"]]
                    :eftest       {:multithread? false}
                    :plugins      [[lein-eftest "0.4.3"]]}})
