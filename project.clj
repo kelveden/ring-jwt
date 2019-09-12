6f74f163ea98e0454082e319235752e70001b745
Alistair Dutton <alistair.dutton@ovoenergy.com>
2019-09-12 10:36:00 +0100

    Tidied up integrant integration a little.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/ring-jwt "1.2.2"
+(defproject ovotech/ring-jwt "1.2.3"
   :description "JWT middleware for Ring"
   :url "http://github.com/ovotech/ring-jwt"
   :license {:name "Eclipse Public License"
--- src/ring/middleware/jwt/integrant.clj
+++ src/ring/middleware/jwt/integrant.clj
@@ -2,7 +2,7 @@
   (:require [integrant.core :as ig]
             [ring.middleware.jwt :refer [wrap-jwt]]))
 
-(defmethod ig/init-key ::jwt [_ {:keys [opts]}]
+(defmethod ig/init-key :ring.middleware.jwt/jwt [_ opts]
   (fn [handler]
     (-> handler
         (wrap-jwt opts))))
