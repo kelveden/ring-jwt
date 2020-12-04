272c40136fca5ede92afdfed12b2c8e2bf234f6d
Alistair Dutton <alistair.dutton@kaluza.com>
2020-12-04 13:13:45 +0000

    Make ring.middleware.jwt/find-token public to allow for fallback when making use of the find-token-fn option.

--- src/ring/middleware/jwt.clj
+++ src/ring/middleware/jwt.clj
@@ -3,7 +3,8 @@
             [ring.middleware.token :as token])
   (:import (com.auth0.jwt.exceptions SignatureVerificationException AlgorithmMismatchException JWTVerificationException TokenExpiredException)))
 
-(defn- find-token*
+(defn find-token
+  "Finds the token by searching the Authorization HTTP header on the incoming request for a bearer token."
   [{:keys [headers]}]
   (some->> headers
            (filter #(.equalsIgnoreCase "authorization" (key %)))
@@ -36,7 +37,7 @@
 
   (fn [req]
     (try
-      (if-let [token ((or find-token-fn find-token*) req)]
+      (if-let [token ((or find-token-fn find-token) req)]
         (if-let [alg-opts (->> token token/decode-issuer (get issuers))]
           (->> (token/decode token alg-opts)
                (assoc req :claims)
