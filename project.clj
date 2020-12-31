cf49470f9b5757b1b0410cdc23ba23032a167fce
Alistair Dutton <alistair.dutton@kaluza.com>
2020-12-31 18:28:15 +0000

    Bumped to latest dependencies.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject ovotech/ring-jwt "2.2.0"
+(defproject ovotech/ring-jwt "2.2.1"
   :description "JWT middleware for Ring"
   :url "http://github.com/ovotech/ring-jwt"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.10.0"]
-                 [commons-codec "1.14"]
+                 [commons-codec "1.15"]
                  [org.clojure/clojure "1.10.1"]
-                 [com.auth0/java-jwt "3.10.3"]
-                 [com.auth0/jwks-rsa "0.12.0"]]
+                 [com.auth0/java-jwt "3.12.0"]
+                 [com.auth0/jwks-rsa "0.15.0"]]
   :profiles {:dev      {:dependencies [[org.clojure/test.check "1.1.0"]
                                        [kelveden/clj-wiremock "1.5.7"]
                                        [org.slf4j/slf4j-simple "1.7.30"]]
