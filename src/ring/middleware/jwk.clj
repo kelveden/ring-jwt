2f36a8f7ab2920e70dab55f10e3fe0116e32d3a3
Alistair Dutton <alistair.dutton@kaluza.com>
2024-04-22 11:03:34 +0100

    Add support for extra headers to jwk provider

--- src/ring/middleware/jwk.clj
+++ src/ring/middleware/jwk.clj
@@ -4,15 +4,16 @@
            (com.auth0.jwt.interfaces RSAKeyProvider)))
 
 (defn- new-jwk-provider
-  [url]
+  [url headers]
   (-> (URL. url)
-      (UrlJwkProvider.)
+      (UrlJwkProvider. nil nil nil headers)
       (GuavaCachedJwkProvider.)))
 
 (def rsa-key-provider
   (memoize
-    (fn [url]
-      (let [jwk-provider (new-jwk-provider url)]
+    (fn [url & [headers]]
+      (let [jwk-provider (new-jwk-provider url (merge {"Accept" "application/json"}
+                                                      (clojure.walk/stringify-keys headers)))]
         (reify RSAKeyProvider
           (getPublicKeyById [_ key-id]
             (-> (.get jwk-provider key-id)
