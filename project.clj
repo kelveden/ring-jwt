b562b3cfbda2729166bd48179c421b5238338a7c
Alistair Dutton <kelveden@gmail.com>
2025-03-11 19:54:44 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.13.0"]
-                 [commons-codec "1.17.1"]
+                 [commons-codec "1.18.0"]
                  [org.clojure/clojure "1.12.0"]
-                 [com.auth0/java-jwt "4.4.0"]
+                 [com.auth0/java-jwt "4.5.0"]
                  [com.auth0/jwks-rsa "0.22.1"]]
   :profiles {:dev      {:dependencies [[org.clojure/test.check "1.1.1"]
-                                       [kelveden/clj-wiremock "1.8.0"]
-                                       [org.slf4j/slf4j-simple "2.0.16"]]
+                                       [kelveden/clj-wiremock "1.9.0"]
+                                       [org.slf4j/slf4j-simple "2.0.17"]]
                         :eftest       {:multithread? false}
                         :plugins      [[lein-eftest "0.4.3"]]}})
