0a22a03b7966fa0c56831eb0e0d7ac968f11110a
Alistair Dutton <kelveden@gmail.com>
2025-03-11 19:55:18 +0000

    Bump to 2.10.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.kelveden/ring-jwt "2.10.2"
+(defproject net.clojars.kelveden/ring-jwt "2.10.3"
   :description "JWT middleware for Ring"
   :url "http://github.com/kelveden/ring-jwt"
   :license {:name "Eclipse Public License"
